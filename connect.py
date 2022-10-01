b4b0322cf6ea0fdefc603ed705d50fefd762d62b
zonbloop <jperaltag1500@alumno.ipn.mx>
2022-10-01 18:53:56 -0500

    Final connection to tables

--- connect.py
+++ connect.py
@@ -8,20 +8,23 @@ def convertToBinaryData(filename):
         binaryData = file.read()
     return binaryData
 
-def insertBLOB(emp_id, name, ubi, photo, vecdataFile, cont, age, dis):
+def insertBLOB(emp_id, name, ubi, photo, vecdataFile, cont, age, dis, table, idp=1, number=1, email="d", parent=1):
     print("Inserting BLOB into parent table")
     try:
-        connection = psycopg.connect("***")
+        connection = psycopg.connect("postgresql://jorge:uJsQkZbKwSMr9fdYM_7NsQ@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dlair-ninja-5391")
         cursor = connection.cursor()
         
-        sql_insert_blob_query = """ INSERT INTO principal (ID, Name, Location, Img, VecImg, Contact, Age, Disability) VALUES (%s,%s,%s,%s,%s,%s,%s,%s) """
-                          
-
-        empPicture = convertToBinaryData(photo)
-        file = convertToBinaryData(vecdataFile)
-
-        # Convert data into tuple format
-        insert_blob_tuple = (emp_id, name, ubi, empPicture, file, cont, age, dis)
+        if table == "principal":
+            sql_insert_blob_query = """ INSERT INTO principal (Id, Name, Location, Img, VecImg, Contact, Age, Disability) VALUES (%s,%s,%s,%s,%s,%s,%s,%s) """
+            empPicture = convertToBinaryData(photo)
+            file = convertToBinaryData(vecdataFile)
+            # Convert data into tuple format
+            insert_blob_tuple = (emp_id, name, ubi, empPicture, file, cont, age, dis)
+        elif table == "secondary":
+            sql_insert_blob_query = """ INSERT INTO secondary (Id, IDp, Name, Location, Number, email, parent) VALUES (%s,%s,%s,%s,%s,%s,%s) """
+            # Convert data into tuple format
+            insert_blob_tuple = (emp_id, idp, name, ubi, number, email, parent)
+        
         result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
         connection.commit()
         print("Image and file inserted successfully as a BLOB into principal table", result)
@@ -34,7 +37,9 @@ def insertBLOB(emp_id, name, ubi, photo, vecdataFile, cont, age, dis):
         connection.close()
         print("Crockroach connection is closed")
 
-insertBLOB(1, "Jorge", "Lafayette 123, Anzures, Alvaro Obregon, CDMX, Mexico", "C:\\Users\\jorge\\Documents\\Hack4ML\\Images\\Jorge.jpg", "C:\\Users\\jorge\\Documents\\Hack4ML\\Images\\Jorgevec.txt", 1, 22, 1)
+insertBLOB(1, "Jorge", "Lafayette 123, Anzures, Alvaro Obregon, CDMX, Mexico",
+ "/home/sara/Documents/AIHack4Good/OpenCV-Face-Recognition-master/FacialRecognition/dataset/User.2.3.jpg",
+ "/home/sara/Documents/AIHack4Good/OpenCV-Face-Recognition-master/FacialRecognition/archivoProp.txt", 1, 22, 1, "principal")
 #cur.execute("select * from disability")
 #res = cur.fetchall()
 #print(res)
