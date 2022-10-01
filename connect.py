ad77f9bf3decb224084332d52b64501dfc6051d9
zonbloop <jperaltag1500@alumno.ipn.mx>
2022-10-01 17:42:59 -0500

    Conversion of image from DB

--- connect.py
+++ connect.py
@@ -1,10 +1,42 @@
 import os
 import psycopg
-conn = psycopg.connect("**")
-with conn.cursor() as cur:
-#    conn.execute("CREATE TABLE secondary( ID int not null, IDp int not null, Name varchar(255), Location varchar(255), Number int not null, Email varchar(100), Parent varchar(50), PRIMARY KEY (ID) )")
-    conn.execute("CREATE TABLE disability( ID int not null, Disability varchar(255), PRIMARY KEY (ID) )")
-    #conn.execute("select * from public.principal")
-    #res = cur.fetchall()
-    conn.commit()
-    #print(res)
+
+
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return binaryData
+
+def insertBLOB(emp_id, name, ubi, photo, vecdataFile, cont, age, dis):
+    print("Inserting BLOB into parent table")
+    try:
+        connection = psycopg.connect("***")
+        cursor = connection.cursor()
+        
+        sql_insert_blob_query = """ INSERT INTO principal (ID, Name, Location, Img, VecImg, Contact, Age, Disability) VALUES (%s,%s,%s,%s,%s,%s,%s,%s) """
+                          
+
+        empPicture = convertToBinaryData(photo)
+        file = convertToBinaryData(vecdataFile)
+
+        # Convert data into tuple format
+        insert_blob_tuple = (emp_id, name, ubi, empPicture, file, cont, age, dis)
+        result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
+        connection.commit()
+        print("Image and file inserted successfully as a BLOB into principal table", result)
+
+    except psycopg.Error as error:
+        print("Failed inserting BLOB data into Crockroach table {}".format(error))
+
+    finally:
+        cursor.close()
+        connection.close()
+        print("Crockroach connection is closed")
+
+insertBLOB(1, "Jorge", "Lafayette 123, Anzures, Alvaro Obregon, CDMX, Mexico", "C:\\Users\\jorge\\Documents\\Hack4ML\\Images\\Jorge.jpg", "C:\\Users\\jorge\\Documents\\Hack4ML\\Images\\Jorgevec.txt", 1, 22, 1)
+#cur.execute("select * from disability")
+#res = cur.fetchall()
+#print(res)
+
+
