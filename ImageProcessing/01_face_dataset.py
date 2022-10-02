27e0452d1ce38edcbc2085b9c035f2286031b4f1
phoebusphi <113799285+phoebusphi@users.noreply.github.com>
2022-10-01 20:27:02 -0500

    update code

--- ImageProcessing/01_face_dataset.py
+++ ImageProcessing/01_face_dataset.py
@@ -1,43 +1,32 @@
-''''
-Capture multiple Faces from multiple users to be stored on a DataBase (dataset directory)
-	==> Faces will be stored on a directory: dataset/ (if does not exist, pls create one)
-	==> Each face will have a unique numeric integer ID as 1, 2, 3, etc                       
-
-Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
-
-Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18    
-
-'''
-
+"""
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18
+Mod. by Fernando M
+"""
 import cv2
 import os
 
-cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video width
-cam.set(4, 480) # set video height
+camera = cv2.VideoCapture(0)
+camera.set(3, 650)
+camera.set(4, 490)
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#Generate id for each person
+face_id = input('\n enter user id end press')
 
-# For each person, enter one numeric face id
-face_id = input('\n enter user id end press <return> ==>  ')
-
-print("\n [INFO] Initializing face capture. Look the camera and wait ...")
-# Initialize individual sampling face count
 count = 0
 
 while(True):
 
-    ret, img = cam.read()
+    ret, img = camera.read()
     img = cv2.flip(img, 1) # flip video image vertically
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    faces = face_detector.detectMultiScale(gray, 1.4, 5)
 
     for (x,y,w,h) in faces:
 
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
         count += 1
 
-        # Save the captured image into the datasets folder
         cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
 
         cv2.imshow('image', img)
@@ -50,7 +39,5 @@ while(True):
 
 # Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
-cam.release()
+camera.release()
 cv2.destroyAllWindows()
-
-
