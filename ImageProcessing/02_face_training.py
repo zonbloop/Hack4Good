7aad476ccb634a029816e36cb307ba7b0af33d49
phoebusphi <113799285+phoebusphi@users.noreply.github.com>
2022-10-01 20:29:02 -0500

    update file

--- ImageProcessing/02_face_training.py
+++ ImageProcessing/02_face_training.py
@@ -1,15 +1,18 @@
+"""
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18
+Mod. by Fernando M
+"""
+
 import cv2
 import numpy as np
 from PIL import Image
 import os
 
-# Path for face image database
 path = 'dataset'
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
 
-# function to get the images and label data
 def getImagesAndLabels(path):
 
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
@@ -30,12 +33,7 @@ def getImagesAndLabels(path):
 
     return faceSamples,ids
 
-print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
 faces,ids = getImagesAndLabels(path)
 recognizer.train(faces, np.array(ids))
 
-# Save the model into trainer/trainer.yml
-recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
-
-# Print the numer of faces trained and end program
-print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+recognizer.write('trainer/trainer.yml')
